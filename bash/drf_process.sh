9d2af76e03a9dd9ae292dc7e602675bbd236b53c
Eric C. Weig <eweig@email.uky.edu>
2017-04-18 09:19:01 -0400

    Update drf_process.sh

--- bash/drf_process.sh
+++ bash/drf_process.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+start='date +%s'
 bash 1800s_process.sh
 echo 1800s processed successfully!
 bash 1900s_process.sh
@@ -14,3 +15,6 @@ echo 1940s processed successfully!
 bash 1950s_process.sh
 echo 1950s processed successfully!
 echo All Daily Racing Form articles have been generated as HTML pages.
+end='date +%s'
+runtime=$((end-start))
+echo $runtime
