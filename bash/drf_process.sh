bb0a1ce56feb2f74b2175e06121e8ee47f8f2bac
Eric C. Weig <eweig@email.uky.edu>
2017-04-18 07:05:02 -0400

    Create drf_process.sh

---
+++ bash/drf_process.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+bash 1800s_process.sh
+echo 1800s processed successfully!
+bash 1900s_process.sh
+echo 1900s processed successfully!
+bash 1910s_process.sh
+echo 1910s processed successfully!
+bash 1920s_process.sh
+echo 1920s processed successfully!
+bash 1930s_process.sh
+echo 1930s processed successfully!
+bash 1940s_process.sh
+echo 1940s processed successfully!
+bash 1950s_process.sh
+echo 1950s processed successfully!
+echo All Daily Racing Form articles have been generated as HTML pages.
